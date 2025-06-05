fa4882caae813f6053ab77617538528f90807fff
Trofel <113066307+lefortnuno@users.noreply.github.com>
2025-06-05 12:08:51 +0100

    Time: 0 ms (100%), Space: 9.3 MB (8.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,18 +1,50 @@
+#include <stdlib.h>
+
+typedef struct {
+    int val;
+    int index;
+} IndexedNum;
+
+int compare(const void* a, const void* b) {
+    return ((IndexedNum*)a)->val - ((IndexedNum*)b)->val;
+}
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* t, int n, int x, int* r) {
-    for (unsigned short int i = 0; i < n; i++) {
-        for (unsigned short int j = i + 1; j < n; j++) {
-            if ((t[i] + t[j]) == x) {
-                int* res = malloc(2 * sizeof(int));
-                res[0] = i;
-                res[1] = j;
-                *r = 2;
-                return res;
-            }
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    IndexedNum* arr = (IndexedNum*)malloc(numsSize * sizeof(IndexedNum));
+    
+    for (int i = 0; i < numsSize; i++) {
+        arr[i].val = nums[i];
+        arr[i].index = i;
+    }
+
+    // Trie les éléments par valeur
+    qsort(arr, numsSize, sizeof(IndexedNum), compare);
+
+    // Deux pointeurs
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left < right) {
+        int sum = arr[left].val + arr[right].val;
+        if (sum == target) {
+            int* res = (int*)malloc(2 * sizeof(int));
+            res[0] = arr[left].index;
+            res[1] = arr[right].index;
+            *returnSize = 2;
+            free(arr);
+            return res;
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
     }
-    *r = 0;
+
+    // Impossible normalement (LeetCode garantit une solution)
+    *returnSize = 0;
+    free(arr);
     return NULL;
 }
